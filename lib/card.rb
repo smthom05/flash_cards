e69d4e7e0b8eafce97509c364535f5595dc9b80f
Scott Thomas <smthom05@gmail.com>
2018-11-28 12:53:58 -0700

    Updated card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,23 +1,13 @@
-require 'pry'
-
 class Card
+  attr_reader :question,
+              :answer,
+              :category
+
   def initialize(question, answer, category)
     @question = question
     @answer = answer
     @category = category
   end
-  def question
-    @question
-  end
-
-  def answer
-    @answer
-  end
-
-  def category
-    @category
-  end
-
 end
 
 card = Card.new("What is Virginia Tech's mascot?", "Hokies", :Sports)
--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,10 @@
 class Turn
+  attr_reader :guess
+              :card
+
   def initialize(string, card)
     @guess = string
     @card = card
-  end
-
-  def guess
-    @guess
-  end
-
-  def card
-    @card
   end
 
   def correct?
