d0614071c149a19384cfff078164a8bc1c0b00d5
Scott Thomas <smthom05@gmail.com>
2018-11-30 18:15:24 -0700

    number_correct and number_correct_by_category methods completed for the round class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,4 @@
 require 'pry'
-require './lib/card'
-require './lib/turn'
 
 class Deck
   attr_reader :cards,
--- lib/round.rb
+++ lib/round.rb
@@ -1,23 +1,45 @@
 require 'pry'
-require './lib/card'
 require './lib/turn'
-require './lib/deck'
-
 
 class Round
-  attr_reader :deck
-  attr_accessor :turns
+  attr_reader :deck,
+              :turns,
+              :correct_answers
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @correct_answers = []
+    @number_correct = 0
   end
 
   def current_card
-
+    @deck.cards[@turns.count]
   end
 
-  def take_turn
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
+
+    return new_turn
   end
 
-end
+  def number_correct
+    @correct_answers = []
+    @turns.each do |turn|
+      if turn.correct?
+      @correct_answers << turn
+      end
+    end
+      @correct_answers.count
+  end
+
+  def number_correct_by_category(category)
+    number_correct
+      correct_answers_by_category =  @correct_answers.find_all do |answer|
+        answer.card.category == category
+      end
+      return correct_answers_by_category.count
+    end
+
+  end
